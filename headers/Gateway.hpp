a4aff009fd63f9b417a611ced696f7505fc21889
Influence631 <kdmitro80@gmail.com>
2025-11-23 13:08:32 +0000

    Added stock struct. Updated Buy and Sell orders and Gateway auth WIP

--- headers/Gateway.hpp
+++ headers/Gateway.hpp
@@ -5,14 +5,33 @@ enum OrderType : uint8_t{
 	Sell = 1,
 };
 
+enum Stock : uint8_t{
+	None = 0,
+	Intel = 1,
+	Palantir = 2,
+	NVIDIA = 3,
+};
+
 struct BuyOrder{
 	uint32_t Volume = 0;
 	int32_t LimitPrice = 0;
+	Stock Stock = None;
 };
 
 struct SellOrder{
 	uint32_t Volume = 0;
 	int32_t LimitPrice = 0;
+	Stock Stock = None;
+};
+
+struct Stock{
+	Stock StockID;
+};
+
+struct User{
+	uint64_t ID;
+	int64_t Balance;
+	unordered_map<Stock, uint64_t> Portfolio;
 };
 
 class Gateway{
@@ -26,14 +45,24 @@ class Gateway{
 };
 
 template<typename OrderT>
-uint64_t Gateway::send_order(const OrderT &order, uint64_t userId){
+uint64_t Gateway::send_order(const OrderT &order, User &user){
 	if constexpr (std::is_same_v<OrderT, BuyOrder>){
+		if(user.Balance >= order.LimitPrice * order.Volume){
+			//eventqueue.add order
+			//user.balance -= volume * limitprice (freeze the money to prevent double buys and race conditions)
+		}
+		else{
+			return 0;
+		}
 		//buy side logic
 		//check sufficient funds
 	}
 	else{
-		//sell side logic
-		//check sufficient shares for user
+		if(user.Portfolio[order.Item] >= order.Volume){
+			//eventqueue.add order
+			//user.Portfolio[order.Item] -= volume. lock the stocks being processed to avoid race con and double selling
+		}
+		else{
+			return 0;
 	}
-	
 }
